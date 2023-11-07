a46cfb866bf1692f600075bb4f458822fbbd098e
garrett0524 <99235575+garrett0524@users.noreply.github.com>
2023-11-07 13:59:16 -0500

    added jsx and css files for Writing page

--- frontend/src/component/WritePost.jsx
+++ frontend/src/component/WritePost.jsx
@@ -1,12 +1,40 @@
-import '../css/PostListItem.css';
+import React, { useState } from 'react';
+import '../css/WritePost.css';
 
-function WritePost() { /* postInfo : PostInfo.js */
-  return (
-   
-    <div className='write_post'>
-        this is writing page
-    </div>
-  );
+function WritePost() {
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+
+    const handlePost = () => {
+        // Action when 'Post' button is clicked
+        // For example: Submit the post data to the backend
+        console.log('Title:', title);
+        console.log('Content:', content);
+        // Add functionality to send the post data to the backend/manage locally
+    };
+
+    return (
+        <div className='write_post'>
+            <div className='input_container'>
+                <input
+                    type='text'
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    placeholder='Title'
+                    className='title_input'
+                />
+                <button onClick={handlePost} className='post_button'>
+                    Post
+                </button>
+            </div>
+            <textarea
+                value={content}
+                onChange={(e) => setContent(e.target.value)}
+                placeholder='Content text holder'
+                className='content_textarea'
+            />
+        </div>
+    );
 }
 
 export default WritePost;
